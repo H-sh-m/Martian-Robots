c55748e0b703b8be562670cd831a2951f3b1af67
Hashim <hashim1234h@gmail.com>
2022-02-24 21:34:33 +0000

    Add robot instruction and position error handling

--- src/main/java/com/hashim/mars/Robot.java
+++ src/main/java/com/hashim/mars/Robot.java
@@ -8,11 +8,19 @@ public class Robot {
     private Grid grid;
 
     public Robot(int currentXPosition, int currentYPosition, String currentOrientation, String instruction, Grid grid) {
-        this.currentXPosition = currentXPosition;
-        this.currentYPosition = currentYPosition;
-        this.currentOrientation = currentOrientation;
-        this.instruction = instruction;
-        this.grid = grid;
+        if(instruction.length() >= 100) {
+            throw new IllegalArgumentException("Instruction string must be less than 100 characters in length.");
+        }
+        else if(currentXPosition > grid.getXLimit() || currentYPosition > grid.getYLimit() || currentXPosition < 0 || currentYPosition < 0) {
+            throw new IllegalArgumentException("Robot position is outside grid bounds.");
+        }
+        else {
+            this.currentXPosition = currentXPosition;
+            this.currentYPosition = currentYPosition;
+            this.currentOrientation = currentOrientation;
+            this.instruction = instruction;
+            this.grid = grid;
+        }
     }
 
     public int getCurrentXPosition() {
--- src/test/java/com/hashim/mars/RobotTest.java
+++ src/test/java/com/hashim/mars/RobotTest.java
@@ -1,9 +1,11 @@
 package com.hashim.mars;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RobotTest {
 
@@ -27,4 +29,89 @@ public class RobotTest {
         assertEquals(robot.getInstruction(), instruction);
         assertEquals(robot.getGrid().getXLimit(), 5);
     }
+
+    /**
+     * All instruction strings will be less than 100 characters in length.
+     */
+    @Test
+    public void givenInstructionsGreaterThan100_whenConstructRobot_thenThrowException() {
+        int initialX = 1, initialY = 1;
+        String initialOrientation = "N";
+        String instruction = "FRRFLLFFRRFLLLLFFFLFLFLRFRFRFRFFRRFLLFFRRFLLLLFFFLFLFLRFRFRFRFFRRFLLFFRRFLLLLFFFLFLFLRFRFRFRFFRRFLLF";
+
+        IllegalArgumentException thrownException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Robot robot = new Robot(initialX, initialY, initialOrientation, instruction, this.grid);
+        });
+
+        assertEquals("Instruction string must be less than 100 characters in length.", thrownException.getMessage());
+    }
+
+    /**
+     * Initial robot X coordinate cannot be negative.
+     */
+    @Test
+    public void givenNegativeInitialXPosition_whenConstructRobot_thenThrowException() {
+        int initialX = -1, initialY = 1;
+        String initialOrientation = "N";
+        String instruction = "RR";
+
+        IllegalArgumentException thrownException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Robot robot = new Robot(initialX, initialY, initialOrientation, instruction, this.grid);
+        });
+
+        assertEquals("Robot position is outside grid bounds.", thrownException.getMessage());
+    }
+
+    /**
+     * Initial robot Y coordinate cannot be negative.
+     */
+    @Test
+    public void givenNegativeInitialYPosition_whenConstructRobot_thenThrowException() {
+        int initialX = 1, initialY = -1;
+        String initialOrientation = "N";
+        String instruction = "RR";
+
+        IllegalArgumentException thrownException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Robot robot = new Robot(initialX, initialY, initialOrientation, instruction, this.grid);
+        });
+
+        assertEquals("Robot position is outside grid bounds.", thrownException.getMessage());
+    }
+
+    /**
+     * Initial robot X coordinate cannot be greater than grid X limit.
+     */
+    @Test
+    public void givenInitialXPositionGreaterThanGridY_whenConstructRobot_thenThrowException() {
+        int initialX = 6, initialY = 1;
+        String initialOrientation = "N";
+        String instruction = "RR";
+
+        assertTrue(initialX > this.grid.getXLimit());
+
+        IllegalArgumentException thrownException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Robot robot = new Robot(initialX, initialY, initialOrientation, instruction, this.grid);
+        });
+
+        assertEquals("Robot position is outside grid bounds.", thrownException.getMessage());
+    }
+
+    /**
+     * Initial robot Y coordinate cannot be greater than grid Y limit.
+     */
+    @Test
+    public void givenInitialYPositionGreaterThanGridY_whenConstructRobot_thenThrowException() {
+        int initialX = 1, initialY = 4;
+        String initialOrientation = "N";
+        String instruction = "RR";
+
+        assertTrue(initialY > this.grid.getYLimit());
+
+        IllegalArgumentException thrownException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Robot robot = new Robot(initialX, initialY, initialOrientation, instruction, this.grid);
+        });
+
+        assertEquals("Robot position is outside grid bounds.", thrownException.getMessage());
+    }
+
 }
